9b8e086c20a0bdaa8ec0b95e575894c0d4f903e8
SahilAslam <sahilaslam14@gmail.com>
2024-08-09 19:30:03 +0530

    added autoplay functionality

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,22 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/Carousel.css";
 
 // eslint-disable-next-line react/prop-types
 const Carousel = ({ countries = [{}] }) => {
   const [current, setCurrent] = useState(0);
+  const [autoPlay, setAutoPlay] = useState(true);
+  let timeOut = null
 
-  const leftClick = () => {
-    setCurrent(current === 0 ? countries.length - 1 : current - 1);   
+  useEffect(() => {
+      timeOut =
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      autoPlay &&
+      setTimeout(() => {
+        slideRight();
+      }, 2500);
+  });
+
+  const slideLeft = () => {
+    setCurrent(current === 0 ? countries.length - 1 : current - 1);
   };
 
-  const rightClick = () => {
+  const slideRight = () => {
     setCurrent(current === countries.length - 1 ? 0 : current + 1);
   };
 
   console.log(current);
 
   return (
-    <div className="carousel">
+    <div
+      className="carousel"
+      onMouseEnter={() => {
+        setAutoPlay(false);
+        clearTimeout(timeOut)
+      }}
+      onMouseLeave={() => {
+        setAutoPlay(true)
+        clearTimeout(timeOut)
+      }}
+    >
       <div className="carousel_wrapper">
         {countries.map((country, index) => (
           <div
@@ -37,11 +58,24 @@ const Carousel = ({ countries = [{}] }) => {
             </div>
           </div>
         ))}
-        <div className="carousel_arrow_left" onClick={leftClick}>
+        <div className="carousel_arrow_left" onClick={slideLeft}>
           &lsaquo;
         </div>
-        <div className="carousel_arrow_right" onClick={rightClick}>
+        <div className="carousel_arrow_right" onClick={slideRight}>
           &rsaquo;
+        </div>
+        <div className="carousel_pagination">
+          {countries.map((_, index) => (
+            <div
+              key={index}
+              className={
+                index == current
+                  ? "pagination_dot pagianation_dot_active"
+                  : "pagination_dot"
+              }
+              onClick={() => setCurrent(index)}
+            ></div>
+          ))}
         </div>
       </div>
     </div>
